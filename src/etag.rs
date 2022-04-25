a73c70e4477cda3ba93a2183ef87fdd733dfc443
Rong Zhou <zhourong@qiniu.com>
2022-04-25 15:28:17 +0800

    add etag_with_parts

--- src/etag.rs
+++ src/etag.rs
@@ -1,15 +1,24 @@
 use super::utils::PythonIoBase;
-use pyo3::prelude::*;
+use pyo3::{prelude::*, types::PyList};
 
 pub(super) fn register(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(etag_of, m)?)?;
+    m.add_function(wrap_pyfunction!(etag_with_parts, m)?)?;
     m.add_function(wrap_pyfunction!(async_etag_of, m)?)?;
+    m.add_function(wrap_pyfunction!(async_etag_with_parts, m)?)?;
     Ok(())
 }
 
 #[pyfunction]
-fn etag_of(io_base: PyObject) -> PyResult<String> {
+fn etag_of(io_base: PyObject, _py: Python<'_>) -> PyResult<String> {
     let etag = qiniu_sdk::etag::etag_of(PythonIoBase::new(io_base))?;
+    Ok(etag)
+}
+
+#[pyfunction]
+fn etag_with_parts(io_base: PyObject, parts: Py<PyList>, py: Python<'_>) -> PyResult<String> {
+    let parts = convert_py_list_to_usize_vec(parts, py)?;
+    let etag = qiniu_sdk::etag::etag_with_parts(PythonIoBase::new(io_base), &parts)?;
     Ok(etag)
 }
 
@@ -20,4 +29,25 @@ fn async_etag_of(io_base: PyObject, py: Python<'_>) -> PyResult<&PyAny> {
             qiniu_sdk::etag::async_etag_of(PythonIoBase::new(io_base).into_async_read()).await?;
         Ok(etag)
     })
+}
+
+#[pyfunction]
+fn async_etag_with_parts(io_base: PyObject, parts: Py<PyList>, py: Python<'_>) -> PyResult<&PyAny> {
+    let parts = convert_py_list_to_usize_vec(parts, py)?;
+    pyo3_asyncio::async_std::future_into_py(py, async move {
+        let etag = qiniu_sdk::etag::async_etag_with_parts(
+            PythonIoBase::new(io_base).into_async_read(),
+            &parts,
+        )
+        .await?;
+        Ok(etag)
+    })
+}
+
+fn convert_py_list_to_usize_vec(parts: Py<PyList>, py: Python<'_>) -> PyResult<Vec<usize>> {
+    parts
+        .as_ref(py)
+        .iter()
+        .map(|pyval| pyval.extract::<usize>())
+        .collect()
 }
--- tests/test_etag.py
+++ tests/test_etag.py
@@ -24,6 +24,27 @@ class TestEtag(unittest.TestCase):
         stream.write(_data_of_size(5 * (1 << 20)))
         stream.seek(0, io.SEEK_SET)
         self.assertEqual(qiniu_rust_bindings.etag_of(stream), 'lg-Eb5KFCuZn-cUfj_oS2PPOU9xy')
+    def test_etag_with_parts(self):
+        stream = io.BytesIO()
+        stream.write(_data_of_size(1 << 20))
+        stream.seek(0, io.SEEK_SET)
+        self.assertEqual(qiniu_rust_bindings.etag_with_parts(stream, [1 << 20]), 'Foyl8onxBLWeRLL5oItRJphv6i4b')
+    def test_middle_size_with_parts(self):
+        stream = io.BytesIO()
+        stream.write(_data_of_size((1 << 19) + (1 << 23)))
+        stream.seek(0, io.SEEK_SET)
+        self.assertEqual(
+            qiniu_rust_bindings.etag_with_parts(stream, [1 << 19, 1 << 23]),
+            'nt82yvMNHlNgZ4H8_A_4de84mr2f'
+        )
+    def test_large_size_with_parts(self):
+        stream = io.BytesIO()
+        stream.write(_data_of_size(9 << 20))
+        stream.seek(0, io.SEEK_SET)
+        self.assertEqual(
+            qiniu_rust_bindings.etag_with_parts(stream, [1 << 22, 1 << 22, 1 << 20]),
+            'ljgVjMtyMsOgIySv79U8Qz4TrUO4'
+        )
 class TestAsyncEtag(unittest.IsolatedAsyncioTestCase):
     async def test_empty_etag_of(self):
         async with aiofiles.tempfile.TemporaryFile('wb+') as f:
@@ -43,6 +64,27 @@ class TestAsyncEtag(unittest.IsolatedAsyncioTestCase):
             await f.write(_data_of_size(5 * (1 << 20)))
             await f.seek(0, io.SEEK_SET)
             self.assertEqual(await qiniu_rust_bindings.async_etag_of(f), 'lg-Eb5KFCuZn-cUfj_oS2PPOU9xy')
+    async def test_etag_with_parts(self):
+        async with aiofiles.tempfile.TemporaryFile('wb+') as f:
+            await f.write(_data_of_size(1 << 20))
+            await f.seek(0, io.SEEK_SET)
+            self.assertEqual(await qiniu_rust_bindings.async_etag_with_parts(f, [1 << 20]), 'Foyl8onxBLWeRLL5oItRJphv6i4b')
+    async def test_middle_size_with_parts(self):
+        async with aiofiles.tempfile.TemporaryFile('wb+') as f:
+            await f.write(_data_of_size((1 << 19) + (1 << 23)))
+            await f.seek(0, io.SEEK_SET)
+            self.assertEqual(
+                await qiniu_rust_bindings.async_etag_with_parts(f, [1 << 19, 1 << 23]),
+                'nt82yvMNHlNgZ4H8_A_4de84mr2f'
+            )
+    async def test_large_size_with_parts(self):
+        async with aiofiles.tempfile.TemporaryFile('wb+') as f:
+            await f.write(_data_of_size(9 << 20))
+            await f.seek(0, io.SEEK_SET)
+            self.assertEqual(
+                await qiniu_rust_bindings.async_etag_with_parts(f, [1 << 22, 1 << 22, 1 << 20]),
+                'ljgVjMtyMsOgIySv79U8Qz4TrUO4'
+            )
 
 def _data_of_size(size):
     buf = []
